55380be03c8de6203934658d73c0d843bcdb800b
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-12-10 12:16:36 +0000

    Create home_view.dart

---
+++ lib/views/home_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/flutter.dart';
+
+class HomeView extends StatelessWidget {
+  HomeView({Key?key}) : super(key: key);
+
+  @override  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Home View'),
+      ),
+    );
+  }
+}
